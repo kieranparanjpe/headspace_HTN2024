699cc2bc8d89c61e66090045ea48d4735ddc891e
sandera <shuang2322@gmail.com>
2024-09-14 21:00:08 -0400

    added text on journal

--- Assets/Scripts/Interaction/Journal.cs
+++ Assets/Scripts/Interaction/Journal.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Linq;
@@ -6,23 +7,67 @@ using UnityEngine;
 public class Journal : Interactable
 {
     private bool journalOpen;
+    private bool canCloseJournal;
     [SerializeField] private Animator animator;
+    [SerializeField] private GameObject pageText;
+
     // Start is called before the first frame update
     void Start()
     {
         journalOpen = false;
+        canCloseJournal = false;
+        pageText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (journalOpen && canCloseJournal)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Interact();
+            }else if (Input.GetKeyDown(KeyCode.G))
+            {
+                Interact();
+                //generate world !!!!!!!! Fill this in
+            }
+        }
     }
 
     protected override void Interact()
     {
+        Debug.Log("hi");
         journalOpen = !journalOpen;
+        Debug.Log(journalOpen);
         animator.SetBool("isOpen", journalOpen);
+        StartCoroutine(FadeText());
 
     }
+    
+    IEnumerator FadeText()
+    {
+
+        if (journalOpen)
+        {
+            yield return new WaitForSeconds(2f);
+            pageText.SetActive(true);
+            canCloseJournal = true;
+        }
+        else
+        {
+            yield return new WaitForSeconds(0f);
+            pageText.SetActive(false);
+            canCloseJournal = false;
+        }
+
+
+    }
+
+    public Boolean isOpen()
+    {
+        return journalOpen;
+    }
+
+
 }
--- Assets/Scripts/Interaction/PlayerInteract.cs
+++ Assets/Scripts/Interaction/PlayerInteract.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class PlayerInteract : MonoBehaviour
 {
     [SerializeField] private Camera cam;
-    [SerializeField] private float distance = 3f;
+    [SerializeField] private float rayDistance;
+    [SerializeField] private Journal journal;
     [SerializeField] private LayerMask mask;
     private PlayerUI playerUI;
     // Start is called before the first frame update
@@ -19,18 +20,19 @@ public class PlayerInteract : MonoBehaviour
     {
         playerUI.UpdateText(string.Empty);
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
-        Debug.DrawRay(ray.origin, ray.direction * distance);
+        Debug.DrawRay(ray.origin, ray.direction * rayDistance);
         RaycastHit hitInfo;
-        if(Physics.Raycast(ray, out hitInfo, distance, mask))
+
+
+        if(Physics.Raycast(ray, out hitInfo, rayDistance, mask))
         {
             Interactable i = hitInfo.collider.GetComponent<Interactable>();
-            if (i != null)
+            if (i != null && !(journal.isOpen()))
             {
                 playerUI.UpdateText(hitInfo.collider.GetComponent<Interactable>().promptMessage);
                 if (Input.GetKeyDown(KeyCode.E))
                 {
                     i.BaseInteract();
-
                 }
             }
         }
